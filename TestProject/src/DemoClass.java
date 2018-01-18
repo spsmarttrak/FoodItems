3d01ca7df4bb560ad1011375e38dbe9e89d4fc9a
swapnil <admin@admin-PC>
2018-01-18 17:13:49 +0530

    This is third commit with new version 0.1

---
+++ TestProject/src/DemoClass.java
@@ -0,0 +1,12 @@
+
+public class DemoClass {
+	
+	public String greeting()
+	{
+		String s = "This is sample string";
+		String s1="welcome to git with eclips";
+		
+		return s1;
+	}
+
+}
--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -1,9 +1,13 @@
 
-public class TestClass {
+public class TestClass  {
 
 	public static void main(String[] args) {
 		
 		System.out.println("main");
+		
+		DemoClass demoClass = new DemoClass();
+		
+		demoClass.greeting();
 
 	}
 
